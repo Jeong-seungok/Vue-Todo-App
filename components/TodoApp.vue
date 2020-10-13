20fa86d3ba74f510e911b10ac8fa0c3707ad63a5
Jeong-seungok <vsnm25@naver.com>
2020-10-13 17:59:13 +0900

    '2020-10-13'

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,13 +1,24 @@
 <template>
     <div>
-        <todo-item />
-        <todo-creator />
+        <todo-item 
+        v-for="todo in todos"
+        :key="todo.id"
+        :todo="todo"
+        @update-todo="updateTodo"
+        @delete-mode="deleteMode"/>
+        <!--todo 속성에 todo 값을 props로 전달해주고 TodoItem은 props를 받아서 내부에서 처리함-->
+        <todo-creator @create-todo="createTodo"/>
+        <!--자식 컴포넌트 TodoCreator에서 전달받은 create-todo 이벤트 연결하여 createTodo 메서드 실행-->
     </div>
 </template>
 
 <script>
 import lowdb from 'lowdb'
 import LocalStorage from 'lowdb/adapters/LocalStorage'
+import cryptoRandomString from 'crypto-random-string'
+import cloneDeep from 'lodash/cloneDeep'
+import _find from 'lodash/find'
+import _assign from 'lodash/assign'
 import TodoCreator from './TodoCreator'
 import TodoItem from './TodoItem'
 
@@ -18,7 +29,8 @@ export default {
     },
     data () {
         return {
-            db: null
+            db: null,
+            todos: []
         }
     },
     // TodoApp.vue가 생성되고 나서 직후, 라이프사이클 훅
@@ -31,10 +43,54 @@ export default {
             const adapter = new LocalStorage('todo-app'); // todo-app이라는 이름의 adapter 생성
             this.db = lowdb(adapter); // lowdb에 adapter를 연결하고 반환되는 값을 this.db에 담는다
 
-            // defaults 메서드로 지정하고 싶은 데이터를 설정
-            this.db.defaults({
+            // db에서 todos 데이터를 가지고 있는지 확인하고 있다면 값을 가져옴
+            // cloneDeep 메서드로 todos 안의 모든 참조관계까지 가져옴
+            // 복사를 하는 이유는 LocalStorage DB의 데이터가 수정되는 것을 막기 위함
+            const hasTodos = cloneDeep(this.db.has('todos').value());
+
+            // todos 데이터가 있는 경우
+            if(hasTodos){
+                this.todos = this.db.getState().todos; // db에서 데이터를 받아와서 todos만 반환
+            }
+            // todos 데이터가 없는 경우
+            else{
+            this.db
+            .defaults({ // defaults 메서드로 지정하고 싶은 데이터를 설정
                 todos: [] // Collection이라 지칭
-            }).write() // lowdb에서는 write 메서드를 마지막에 추가해야 실제 DB에 작성된다
+            })
+            .write() // lowdb에서는 write 메서드를 마지막에 추가해야 실제 DB에 작성된다
+            }
+        },
+        // Todo 생성
+        // 매개변수 title은 TodoCreator 컴포넌트의 this.title이다
+        createTodo(title){
+            // 객체 리터럴 생성(양식 개념)
+            const newTodo = {
+                id: cryptoRandomString({ length: 10 }),
+                title, // = title: title 속성의 이름과 변수명이 같으면 하나로 생략 가능
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                done: false
+            }
+
+            // 
+            this.db
+            .get('todos') // lodash에서 제공하는 메서드 get,push
+            .push(newTodo)
+            .write() // lowdb에서 제공하는 메서드 write(갱신) 필수
+        },
+        updateTodo(todo, value){
+            this.db
+            .get('todos')
+            .find({id:todo.id}) // 첫번째 인수로 올 검색할 배열은 위에 get에서 받아오기에 생략 가능
+            .assign(value) // value 객체 데이터를 갱신
+            .write()
+
+            const foundTodo = _find(this.todos, {id: todo.id});
+            _assign(foundTodo, value);
+        },
+        deleteMode(){
+            
         }
     }
 }
--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,9 +1,129 @@
 <template>
-    <h2>Todo Item</h2>
+    <div class="todo-items">
+        <!-- 수정모드 활성화 -->
+        <div 
+        class="item__inner item--edit"
+        v-if="iseditMode">
+            <input 
+            type="text"
+            :value="editedTitle"
+            ref="editModeTitle"
+            @input="editedTitle = $event.target.value"
+            @keypress.enter="completededit"
+            @keypress.esc="offeditMode">
+            <div class="item__actions">
+                <!-- key값으로 다른 엘리먼트 포커싱 방지 -->
+                <button 
+                key="complete"
+                @click="completededit">완료</button>
+                <button 
+                key="cancel"
+                @click="offeditMode">취소</button>
+            </div>
+        </div>
+        <!-- 기본 형태 -->
+        <div 
+        class="item__inner item--normal"
+        v-else>
+
+            <input 
+            v-model="done" 
+            type="checkbox" />
+            <!-- 제목 날짜 -->
+            <div class="item__title-wrap">
+                <div class="item__title">
+                    {{ todo.title }}
+                </div>
+                <div class="item__date">
+                    {{ date }}
+                </div>
+            </div>
+            <!-- 수정, 삭제 -->
+            <div class="item__actions">
+                <!-- key값으로 다른 엘리먼트 포커싱 방지 -->
+                <button 
+                key="update"
+                @click="editMode">수정</button>
+                <button 
+                key="delete"
+                @click="deleteMode">삭제</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import dayjs from 'dayjs' // 날짜 수정을 위한 라이브러리
+
 export default {
-    
+    // props에 대한 요구사항 지정(검증)
+    props: {
+        todo: Object // TodoApp 부모컴포넌트에서 받은 todo를 담은 속성 todo
+    },
+    data (){
+        return {
+            iseditMode: false, // 수정모드 기본 값은 false
+            editedTitle: '' // 수정모드 title 기본 값은 ''
+        }
+    },
+    // 가공된 데이터
+    computed: {
+        // todo는 DB의 데이터를 복사한 것이기 때문에 수정을 해도 반영이 안됨을 해결하기 위한 done 옵션
+        done: {
+            // done이 실행되기 위한 특정 데이터 상태를 가져옴
+            get(){ 
+                return this.todo.done;
+            },
+            // done이 변경되었을때 어떻게 처리할지 결정
+            // 변경된 값을 매개변수로 받을 수 있음
+            set(done){
+                console.log(done);
+                this.updateTodo({done})
+            }
+        },
+        date(){
+            const data = dayjs(this.todo.createdAt);
+            const isSame = data.isSame(this.todo.updatedAt);
+
+            if(isSame){
+                return data.format('YYYY년 MM월 DD일');
+            }
+            else{
+                return `${data.format('YYYY년 MM월 DD일')} (edited)` 
+            }
+        }
+    },
+    methods:{
+        completededit(){
+            // 수정내용과 기존 내용이 다를 경우에만 업데이트
+            if(this.todo.title !== this.editedTitle){
+                this.updateTodo({
+                    title: this.editedTitle,
+                    updatedAt: new Date()
+                });
+                this.offeditMode()
+            }
+        },
+        // 수정할때
+        editMode(){
+            this.iseditMode = true; // 수정모드 활성화
+            this.editedTitle = this.todo.title; // input 값 = 해당 todo의 title 값
+
+        // 수정모드 활성시 input에 포커싱
+            this.$nextTick(()=>{ // nextTick은 뷰에서 제공하는 메서드(DOM이 업데이트 되고나서 실행)
+                this.$refs.editModeTitle.focus(); 
+            })
+        },
+        // 수정종료했을때
+        offeditMode(){
+            this.iseditMode = false;
+        },
+        deleteMode(){
+            this.$emit('delete-mode', this.todo); // delete-mode 이벤트 이름과 해당 todo를 전달
+        },
+        updateTodo(value){
+            this.$emit('update-todo', this.todo, value); // update-todo 이벤트 이름, 해당 todo, {done}
+        }
+    }
 }
 </script>
